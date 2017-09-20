1049f20ee74728b8fee41fb92972663e5de8bbe8
wubingle <wu.bingle@ucsmy.com>
2017-09-20 17:42:17 +0800

    修改返回箭头

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,7 +50,8 @@ export class MyApp {
         }
 
         this.translate.get(['BACK_BUTTON_TEXT']).subscribe(values => {
-            this.config.set('ios', 'backButtonText', values.BACK_BUTTON_TEXT);
+            // this.config.set('ios', 'backButtonText', values.BACK_BUTTON_TEXT);
+            this.config.set('ios', 'backButtonText', '');
         });
     }
 
